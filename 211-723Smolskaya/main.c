2ee304472b7c00ee08a7fae2194922256e9c5fed
Vimachka <smola_chikpetals19@mail.ru>
2021-09-21 00:08:27 +0300

    Initial Commit

---
+++ 211-723Smolskaya/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  211-723Smolskaya
+//
+//  Created by Виктория  Смольская  on 21.09.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
