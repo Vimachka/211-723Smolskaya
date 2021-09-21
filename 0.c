add84146871ca52b6b7a11463435fe586ecdcd38
Vimachka <91091212+Vimachka@users.noreply.github.com>
2021-09-21 11:45:29 +0300

    Add files via upload

---
+++ 0.c
@@ -0,0 +1,22 @@
+//
+//  0.c
+//  211-723Smolskaya
+//
+//  Created by Виктория  Смольская  on 21.09.2021.
+//
+
+#include <stdio.h>
+
+int main () {
+    int a = 0;
+    printf("Введите длину прямоугольника:");
+    scanf("%d", &a);
+    
+    int b = 0;
+    printf ("Введите ширину прямогольника:");
+    scanf("%d", &b);
+    int s = 0;
+    s = a*b;
+    printf ("Площадь прямоугольника = %d\n", s);
+    return 0;
+}
