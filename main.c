84fc74798bfa4550b6c16661531f3ca5f14bc5fb
Vimachka <91091212+Vimachka@users.noreply.github.com>
2021-09-21 12:09:50 +0300

    №2

---
+++ main.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  211-237 #2
+//
+//  Created by Виктория  Смольская  on 21.09.2021.
+//
+
+#include <stdio.h>
+
+int main() {
+
+    
+    
+    float a = 0;
+    printf("Введите длину диаметра:");
+    scanf("%f", &a);
+   float b = 3.14;
+    double l = 0;
+    l = a*b;
+    printf ("длина окружности = %.7f\n", l);
+    return 0;
+}
+ 
+    
+
+
